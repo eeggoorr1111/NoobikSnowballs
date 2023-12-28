0919132f41f6d59df888338d9b10777e71660db4
Egor Sychev <egormywork@gmail.com>
2023-12-28 23:37:26 +0200

    Взрыв криперов при столкновении

---
+++ Assets/_ProjectAssets/Scripts/Enemies/Creeper/CreeperDeathExplosion.cs
@@ -0,0 +1,16 @@
+﻿using Narratore.Solutions.Battle;
+using UnityEngine;
+
+namespace Narratore.DI
+{
+    public class CreeperDeathExplosion : ExplosionUnitDeath
+    {
+        public void Death()
+        {
+            /// Немного костыль) Нужно чтобы при самоуничтожении первый раз отправлялось событие о взрыве
+            IShootingKillable shootingKillable = this;
+            shootingKillable.Death(Vector3.zero);
+        }
+    }
+}
+
