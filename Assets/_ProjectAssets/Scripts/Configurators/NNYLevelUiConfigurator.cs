454f10dd31bc21d57595bc296055303c1420ab31
Egor Sychev <egormywork@gmail.com>
2023-12-28 23:37:38 +0200

    Конфигуратор UI

---
+++ Assets/_ProjectAssets/Scripts/Configurators/NNYLevelUiConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using Narratore.Pools;
+using Narratore.UI;
+using UnityEngine;
+using VContainer;
+
+namespace Narratore.DI
+{
+    public class NNYLevelUiConfigurator : Configurator
+    {
+        [SerializeField] private ShopWindow _shopWindow;
+
+
+        public override void Configure(IContainerBuilder builder, LevelConfig config, SampleData sampleData)
+        {
+            builder.RegisterInstance(_shopWindow);
+        }
+    }
+}
+
